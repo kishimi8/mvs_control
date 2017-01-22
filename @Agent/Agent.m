71cf368c320d9beaafb0ccb926e8861647557ce8
Mario Selvaggio <mario.selvaggio@iit.it>
2017-01-23 00:53:21 +0100

    class files created

---
+++ @Agent/Agent.m
@@ -0,0 +1,19 @@
+classdef Agent
+
+    %% PROPERTIES
+    properties
+
+    end
+    
+	%% METHODS
+    methods
+    	
+		%% COSTRUCTOR
+        function obj = Agent()
+             end
+    
+        function Example(obj)
+	     end
+
+    end
+
---
+++ @System/System.m
@@ -0,0 +1,18 @@
+classdef System < handle
+
+    %% PROPERTIES
+    properties
+
+    end
+
+	%% METHODS	
+    methods
+   	 	%% COSTRUCTOR
+        function obj = System()
+             end
+
+        function Example(obj)
+	         end
+
+    end
+
